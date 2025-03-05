93a3513fc8157335fc08e61f8323cdf09b096c92
Dasha-helo <Dashask2001@gmail.com>
2025-03-05 18:06:09 +0300

    Add files via upload

---
+++ barashka-лепёшка/ТОЛЧОК/lesha.js
@@ -0,0 +1,12 @@
+document.getElementById("login-btn").addEventListener('click',function(){
+    
+const loginInput = document.getElementById('login').value
+const passwordInput = document.getElementById('password').value
+let login = "user"
+let password = "password"
+if (loginInput===login&&passwordInput===password){
+    window.open('ЛЕПЁХА.html')  
+} else{
+
+}
+})
